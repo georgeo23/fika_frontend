e2e95d36b483b2d972b7ec05803547c45ee6fac7
georgeo23 <olney.george@yahoo.co.uk>
2020-10-02 09:13:16 +0100

    front end

--- src/components/GameChatMessage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default ({ name, message, time }) => (
-  <div className="otherGameUsers">
-    <div>
-      <strong>{name}</strong>
-    </div>{" "}
-    {message}
-  </div>
-);
--- src/components/GameUserMessage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default ({ name, message, time }) => (
-  <div className="userGameMessage">
-    <div>
-      <strong>{name}</strong>
-    </div>{" "}
-    {message}
-  </div>
-);
